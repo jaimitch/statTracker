33648930ade30446f8b1bae4ea6be63a58654756
Jaren Mitchell <jaren_mitchell@yahoo.com>
2017-08-24 00:56:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mustache = require('express-mustache');
+const mongoose = require('mongoose');
+const session = require('express-session');
+mongoose.Promise = require('bluebird');
+
+
+mongoose.connect('mongodb://localhost:27017/activityTracker');
+
+
+const index = require('./routes/index');
+const api = require('./routes/api');
+
+
+const application = express();
+
+
+application.engine('mustache', mustache({ defaultLayout: 'main' }));
+application.set('view engine', 'mustache');
+
+application.use('/assets', express.static(path.join(__dirname, 'public')))
+
+// parse applicationlication/x-www-form-urlencoded
+application.use(bodyParser.urlencoded({ extended: false }))
+
+// parse applicationlication/json
+application.use(bodyParser.json());
+
+application.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+
+application.use(function(request,response, next){
+
+  if (!request.session.isAuthenticated){
+      request.session.isAuthenticated = false;
+  }
+  next();
+});
+
+
+application.use('/api', api);
+application.use('/', index);
+
+
+application.listen(3000, function () {
+    console.log('Server listening on port 8000');
+});
---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    dateJoined: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    activities: [{type: mongoose.Schema.ObjectId, ref: "Activity"}],
+   
+});
+
+const User = mongoose.model('User', userSchema );
+
+module.exports = User;
